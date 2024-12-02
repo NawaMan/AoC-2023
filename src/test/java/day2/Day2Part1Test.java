c27fefedddcafa5d7c5c4d8549e91d37e2c286f7
Nawa Manusitthipol <nawa@nawaman.net>
2024-12-01 22:18:06 -0500

    Day 2

--- src/test/java/day2/Day2Part1Test.java
+++ src/test/java/day2/Day2Part1Test.java
@@ -3,19 +3,18 @@ package day2;
 import static day2.Day2Part1Test.Color.blue;
 import static day2.Day2Part1Test.Color.green;
 import static day2.Day2Part1Test.Color.red;
+import static functionalj.lens.Access.theList;
+import static functionalj.lens.Access.theString;
 import static functionalj.list.FuncList.AllOf;
 import static functionalj.tuple.Tuple.tuple2;
-import static org.junit.Assert.fail;
+import static java.lang.Boolean.TRUE;
+import static java.lang.Integer.parseInt;
 
-import java.util.Arrays;
-
-import org.junit.Ignore;
 import org.junit.Test;
 
 import common.BaseTest;
 import functionalj.list.FuncList;
 import functionalj.map.FuncMap;
-import functionalj.tuple.Tuple;
 import functionalj.tuple.Tuple2;
 
 public class Day2Part1Test extends BaseTest {
@@ -32,65 +31,33 @@ public class Day2Part1Test extends BaseTest {
                 blue,  14
             );
     
-    private int calculateSum(FuncList<String> lines) {
-        System.out.println("Inputs: ");
+    int calculateSum(FuncList<String> lines) {
+        var sum = lines
+              .map          (theString.split(" *: *"))
+              .toMap        (theList.at(0).asString(), 
+                             theList.at(1).asString())
+              .mapValue     (this::valueToPairs)
+              .mapValue     (pairs -> pairs.allMatch(this::validatePair))
+              .filterByValue(TRUE::equals)
+              .keys         ()
+              .map          (theString.replaceAll("Game ", ""))
+              .mapToInt     (Integer::parseInt)
+              .sum();
         
-        for (var line : lines) {
-            var parts = line.split(":");
-            var gameStr = parts[0].trim();
-            var mapStr  = parts[1].trim();
-            
-            var coinPairs = valueToPair(mapStr);
-            var pairs     = processPair(coinPairs);
-            
-            var valid = validateCount(pairs, maxByColors);
-            
-            if (valid) {
-                System.out.println("    " + gameStr + ": " + valid + " <- " +  pairs + " -- " + mapStr + " -- " + coinPairs);
-            }
-        }
-        
-        
-        
-//        lines.map(indent()).forEach(System.out::println);
-//        
-//        var map = lines
-//                .toMap   (line   -> line.split(":")[0], 
-//                        line   -> line.split(":")[1].trim())
-//              .mapValue(value  -> valueToPair(value))
-//              .mapValue(pairs  -> processPair(pairs));
-//        map.entries().forEach(entry -> {
-//            println(entry.getKey() + ": " + entry.getValue() + " = " + validateCount(entry.getValue(), maxByColors));
-//        });
-//        var sum = map
-//        .mapValue(value -> validateCount(value, maxByColors))
-//        .filterByValue(Boolean.TRUE::equals)
-//        .keys()
-//        .map(key -> {
-//            println(key + ": " + map.get(key));
-//            return key;
-//        })
-//        .map(key -> key.replaceAll("[^0-9]*", ""))
-//        .mapToInt(Integer::parseInt)
-//        .peek(this::println)
-//        .sum();
-        
-        int sum = 0;
         return sum;
     }
     
-    private FuncList<Tuple2<String, Integer>> valueToPair(String value) {
-        return AllOf(value.split("[,;]"))
+    FuncList<Tuple2<String, Integer>> valueToPairs(String value) {
+        return AllOf(value.split(" *[,;] *"))
                 .map(each -> each.trim().split(" "))
-                .map(each -> tuple2(each[1], Integer.parseInt(each[0])));
+                .map(each -> tuple2(each[1], parseInt(each[0])));
     }
     
-    private FuncMap<Color, Integer> processPair(FuncList<Tuple2<String, Integer>> pairs) {
-        return pairs.toMap(pair -> Color.valueOf(pair.first()), pair -> pair.second(), (a, b) -> a + b);
-    }
-    
-    private boolean validateCount(FuncMap<Color, Integer> colors, FuncMap<Color, Integer> maxByColors) {
-        return AllOf(Color.values()).allMatch(color -> colors.getOrDefault(color, 0) <= maxByColors.getOrDefault(color, 0));
+    boolean validatePair(Tuple2<String, Integer> pair) {
+        var color = Color.valueOf(pair.first());
+        var count = pair.second();
+        var max   = maxByColors.getOrDefault(color, 0);
+        return count <= max;
     }
     
     //== Test ==
@@ -99,10 +66,6 @@ public class Day2Part1Test extends BaseTest {
     public void testExample() {
         var lines = readAllLines();
         var sum = calculateSum(lines);
-        
-        println();
-        println(sum);
-        
         assertAsString("8", sum);
     }
     
@@ -110,11 +73,7 @@ public class Day2Part1Test extends BaseTest {
     public void testProd() {
         var lines = readAllLines();
         var sum = calculateSum(lines);
-        
-        println();
-        println(sum);
-        
-//        assertAsString("", ...);
+        assertAsString("2486", sum);
     }
     
 }
--- src/test/java/day2/Day2Part2Test.java
+++ src/test/java/day2/Day2Part2Test.java
@@ -1,38 +1,64 @@
 package day2;
 
-import static org.junit.Assert.fail;
+import static functionalj.lens.Access.theList;
+import static functionalj.lens.Access.theString;
+import static functionalj.list.FuncList.AllOf;
+import static functionalj.tuple.Tuple.tuple2;
+import static java.lang.Integer.parseInt;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
-import common.AocCommon;
-import common.Testable;
+import common.BaseTest;
+import functionalj.list.FuncList;
+import functionalj.tuple.Tuple2;
 
-@Ignore
-public class Day2Part2Test implements AocCommon, Testable {
-
+public class Day2Part2Test extends BaseTest {
+    
+    static enum Color {
+        red,
+        green,
+        blue;
+    }
+    
+    int calculate(FuncList<String> lines) {
+        return lines
+              .map     (theString.split(" *: *"))
+              .toMap   (theList.at(0).asString(), theList.at(1).asString())
+              .mapValue(this::valueToPairs)
+              .mapValue(this::calculatePower)
+              .values  ()
+              .mapToInt(Integer::intValue)
+              .sum();
+    }
+    
+    FuncList<Tuple2<String, Integer>> valueToPairs(String value) {
+        return AllOf(value.split(" *[,;] *"))
+                .map(each -> each.trim().split(" "))
+                .map(each -> tuple2(each[1], parseInt(each[0])));
+    }
+    
+    private Integer calculatePower(FuncList<Tuple2<String, Integer>> pairs) {
+        return pairs.toMap(
+                    pair -> pair.first(), 
+                    pair -> pair.second(), 
+                    Math::max).values()
+                .reduce((a, b) -> a*b).get();
+    }
     
     //== Test ==
     
     @Test
-    public void testDemo() {
-        var lines = readAllLines(example, challengeName());
-        System.out.println(lines);
-        
-        
-        //assertAsString("", ...);
-        fail("No assertion!");
+    public void testExample() {
+        var lines = readAllLines();
+        lines.forEach(this::println);
+        assertAsString("2286", calculate(lines));
     }
     
     @Test
     public void testProd() {
-        var lines = readAllLines(prod, challengeName());
-        System.out.println(lines);
-        
-        
-        
-        //assertAsString("", ...);
-        fail("No assertion!");
+        var lines = readAllLines();
+        lines.forEach(this::println);
+        assertAsString("87984", calculate(lines));
     }
     
 }
